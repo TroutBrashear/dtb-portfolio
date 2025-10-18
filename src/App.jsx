31cad2f8516c12c5beeb13d44878cca4b6ce0bb1
TroutBrashear <215436580+TroutBrashear@users.noreply.github.com>
2025-10-18 19:32:00 +0200

    feat: additional content on tech stack and projects

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,21 @@ function App() {
         <h1>Dylan Trout Brashear</h1>
         <h2>Full-Stack Software Engineer</h2>
         <p>I build user-centric web applications with a focus on unique systems experiences. I am specialized in the React ecosystem.</p>
+        <h2>Tech Stack</h2>
+        <ul>
+          <li>React</li>
+          <li>Zustand</li>
+          <li>Vite</li>
+          <li>Vitest</li>
+          <li>Vercel</li>
+          <li></li>
+        </ul>
+        <h2>Projects</h2>
+        <h3>Terroraize</h3>
+        <p>Terroraize is a structured AI storytelling application built in React. It permits users to request AI written scenes using user-defined locations and characters as building blocks. It offers user-defined character memory and goals, adjustable prompts, and an AI director feature that suggests the next scenes.</p>
+
+        <h3>Void Crowns</h3>
+        <p>Void Crowns is a complex web browser strategy game built in React.</p>
       </div>
   );
 }
